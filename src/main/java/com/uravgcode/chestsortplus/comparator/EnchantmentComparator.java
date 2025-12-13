8dc7351a40c82be9a401db778da5eb7734d0d9ec
UrAvgCode <dev@uravgcode.com>
2025-12-13 16:55:53 +0100

    add enchantment comparator

---
+++ src/main/java/com/uravgcode/chestsortplus/comparator/EnchantmentComparator.java
@@ -0,0 +1,31 @@
+package com.uravgcode.chestsortplus.comparator;
+
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class EnchantmentComparator implements Comparator<EnchantmentStorageMeta> {
+
+    @Override
+    public int compare(EnchantmentStorageMeta o1, EnchantmentStorageMeta o2) {
+        final var enchantments1 = List.copyOf(o1.getStoredEnchants().entrySet());
+        final var enchantments2 = List.copyOf(o2.getStoredEnchants().entrySet());
+
+        int sizeOrder = Integer.compare(enchantments1.size(), enchantments2.size());
+        if (sizeOrder != 0) return sizeOrder;
+
+        for (int i = 0; i < enchantments1.size(); ++i) {
+            final var entry1 = enchantments1.get(i);
+            final var entry2 = enchantments2.get(i);
+
+            int enchantmentOrder = entry1.getKey().key().compareTo(entry2.getKey().key());
+            if (enchantmentOrder != 0) return enchantmentOrder;
+
+            int levelOrder = Integer.compare(entry1.getValue(), entry2.getValue());
+            if (levelOrder != 0) return levelOrder;
+        }
+
+        return 0;
+    }
+}
--- src/main/java/com/uravgcode/chestsortplus/comparator/ItemComparator.java
+++ src/main/java/com/uravgcode/chestsortplus/comparator/ItemComparator.java
@@ -2,16 +2,19 @@ package com.uravgcode.chestsortplus.comparator;
 
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 
 import java.util.Comparator;
 
 public final class ItemComparator implements Comparator<ItemStack> {
     private final MaterialComparator materialComparator;
+    private final EnchantmentComparator enchantmentComparator;
     private final PotionComparator potionComparator;
 
     public ItemComparator() {
         this.materialComparator = new MaterialComparator();
+        this.enchantmentComparator = new EnchantmentComparator();
         this.potionComparator = new PotionComparator();
     }
 
@@ -19,6 +22,11 @@ public final class ItemComparator implements Comparator<ItemStack> {
     public int compare(ItemStack o1, ItemStack o2) {
         final var materialOrder = materialComparator.compare(o1.getType(), o2.getType());
         if (materialOrder != 0) return materialOrder;
+
+        if (o1.getItemMeta() instanceof EnchantmentStorageMeta meta1 && o2.getItemMeta() instanceof EnchantmentStorageMeta meta2) {
+            final var enchantmentOrder = enchantmentComparator.compare(meta1, meta2);
+            if (enchantmentOrder != 0) return enchantmentOrder;
+        }
 
         if (o1.getItemMeta() instanceof PotionMeta meta1 && o2.getItemMeta() instanceof PotionMeta meta2) {
             final var potionOrder = potionComparator.compare(meta1.getBasePotionType(), meta2.getBasePotionType());
